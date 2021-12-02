4299715778b0155adafbb86336db3d4d3e3855c1
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2021-12-02 08:47:26 +0100

    day 2 part 1

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,2 +1,28 @@
+import os
+
+
+def get_final_depth(input_file):
+    horizontal_pos = 0
+    depth = 0
+
+    with open(input_file) as file:
+        lines = file.readlines()
+        line_list = [line.rstrip() for line in lines]
+
+    for elem in line_list:
+        keyword, key_num = elem.split()
+        key_num = int(key_num)
+        if keyword == "forward":
+            horizontal_pos += key_num
+        elif keyword == "down":
+            depth += key_num
+        elif keyword == "up":
+            depth -= key_num
+
+    return horizontal_pos * depth
+
+
 if __name__ == "__main__":
-    pass
+    my_input_file = os.path.abspath("./input_day_2.txt")
+
+    print(get_final_depth(my_input_file))
--- day_2/tests/test_day_2.py
+++ day_2/tests/test_day_2.py
@@ -1,2 +1,10 @@
+import os
+import day_2.day_2
+
+
 def test_day_2():
-    assert True
+    input_file = os.path.abspath("./test_input_day_2.txt")
+    return_val = day_2.day_2.get_final_depth(input_file)
+
+    assert isinstance(return_val, int)
+    assert return_val == 150
