3ca285ba8174848549170369131a9f9c56761046
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2021-12-07 14:02:30 +0100

    day 3 part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -24,12 +24,75 @@ def get_power_consumption(input_file):
         else:
             gamma += "1"
             epsilon += "0"
-    print(gamma)
-    print(epsilon)
+    # print(gamma)
+    # print(epsilon)
     return int(gamma, 2) * int(epsilon, 2)
+
+
+def get_oxygen_rating(list_of_diagnostics):
+    data_len = len(list_of_diagnostics[0])
+
+    for i in range(0, data_len):
+        # true_count count '0'
+        true_count = 0
+        # false_count count '1'
+        false_count = 0
+        for line in list_of_diagnostics:
+            if int(line[i]) == 0:
+                true_count += 1
+            else:
+                false_count += 1
+        for line in list_of_diagnostics:
+            if true_count > false_count:
+                if int(line[i]) == 1:
+                    list_of_diagnostics = [x for x in list_of_diagnostics if x != line]
+            else:
+                if int(line[i]) == 0:
+                    list_of_diagnostics = [x for x in list_of_diagnostics if x != line]
+        if len(list_of_diagnostics) == 1:
+            break
+
+    return int(list_of_diagnostics[0], 2)
+
+
+def get_co2_rating(list_of_diagnostics):
+    data_len = len(list_of_diagnostics[0])
+
+    for i in range(0, data_len):
+        # true_count count '0'
+        true_count = 0
+        # false_count count '1'
+        false_count = 0
+        for line in list_of_diagnostics:
+            if int(line[i]) == 0:
+                true_count += 1
+            else:
+                false_count += 1
+
+        for line in list_of_diagnostics:
+            if true_count > false_count:
+                if int(line[i]) == 0:
+                    list_of_diagnostics = [x for x in list_of_diagnostics if x != line]
+            else:
+                if int(line[i]) == 1:
+                    list_of_diagnostics = [x for x in list_of_diagnostics if x != line]
+        if len(list_of_diagnostics) == 1:
+            break
+
+    return int(list_of_diagnostics[0], 2)
+
+
+def get_life_support_rate(input_file):
+    file_list = helper.get_file_as_str_list(input_file)
+
+    oxygen = get_oxygen_rating(file_list.copy())
+    co2 = get_co2_rating(file_list.copy())
+
+    return oxygen * co2
 
 
 if __name__ == "__main__":
     my_input_file = os.path.abspath("./input_day_3.txt")
 
     print(get_power_consumption(my_input_file))
+    print(get_life_support_rate(my_input_file))
--- day_3/tests/test_day_3.py
+++ day_3/tests/test_day_3.py
@@ -1,11 +1,30 @@
 import os
 import day_3.day_3
+import helper
+
+input_file = os.path.abspath("./test_input_day_3.txt")
+input_list = helper.get_file_as_str_list(input_file)
 
 
 def test_day_3_part_1():
-    input_file = os.path.abspath("./test_input_day_3.txt")
 
     test_power = day_3.day_3.get_power_consumption(input_file)
 
     assert isinstance(test_power, int)
     assert test_power == 198
+
+
+def test_day_3_part_2_oxygen():
+
+    test_oxygen = day_3.day_3.get_oxygen_rating(input_list.copy())
+
+    assert isinstance(test_oxygen, int)
+    assert test_oxygen == 23
+
+
+def test_day_3_part_2_co2():
+
+    test_co2 = day_3.day_3.get_co2_rating(input_list.copy())
+
+    assert isinstance(test_co2, int)
+    assert test_co2 == 10
