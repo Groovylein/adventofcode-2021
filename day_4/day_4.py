bdca9318b90a300024537b81fc478a36b12c705b
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2021-12-08 11:55:05 +0100

    import file for day 4

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,4 +1,10 @@
 import os
+import helper
+
 
 if __name__ == "__main__":
     my_input_file = os.path.abspath("./input_day_4.txt")
+
+    draw_list, board_list = helper.get_file_for_bingo(my_input_file)
+    # print("draw:", draw_list)
+    # print("board:", board_list)
--- helper.py
+++ helper.py
@@ -3,3 +3,19 @@ def get_file_as_str_list(input_file):
         lines = file.readlines()
         line_list = [line.rstrip() for line in lines]
     return line_list
+
+
+def get_file_for_bingo(input_file):
+    board_list = []
+    draw_list = []
+    with open(input_file) as file:
+        lines = file.readlines()
+        for idx in range(0, len(lines)):
+            if idx == 0:
+                draw_list = lines[idx].rstrip().split(",")
+            elif not lines[idx].rstrip():
+                continue
+            else:
+                board_list.append([line.rstrip() for line in lines[idx:(idx + 4)]])
+                idx += 4
+    return draw_list, board_list
